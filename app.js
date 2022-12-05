f03278f35f724b919dbca52d9499dfda904ae5cd
Jim Rudolf <rudolf@jbrcodes.com>
2022-12-05 16:11:45 +0100

    Try to force a push!!

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Try to force a push!!!
 // app.use(express.static(path.join(__dirname, "public")));
 
 // Location of static assets
